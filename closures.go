bad59bfaf7263220558405f6b72c4c4b8cb1873c
Philip Graf <mail@philipgraf.de>
2013-12-04 01:05:00 +0100

    Add closure example

---
+++ closures.go
@@ -0,0 +1,17 @@
+package main
+import "fmt"
+
+func add() func(x int) int{
+  sum := 0
+  return func(x int) int{
+    sum += x
+    return sum
+  }
+}
+
+func main(){
+  x,y := add(), add()
+  for i:=0;i<10;i++{
+    fmt.Println(x(i), y(-2*i))
+  }
+}
